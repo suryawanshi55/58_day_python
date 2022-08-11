ca2d1f064e44e3a02fe79573ecc243d0785bc377
suryawanshi55 <107236171+suryawanshi55@users.noreply.github.com>
2022-08-11 22:07:36 +0530

    Add files via upload

---
+++ polymorphism.py
@@ -0,0 +1,18 @@
+# class animal:
+#     def noise(self):
+#         pass
+# class cat(animal):
+#     def noise(self):
+#         print('meoow')
+# class dog(animal):
+#     def noise(self):
+#         print("woof")
+# class monkey(animal):
+#     def noise(self):
+#         print("urrr")
+# a=cat()
+# a.noise()        # meoow
+# a=dog()
+# a.noise()        # woof
+# a=monkey()
+# a.noise()        # urrr
